cc47fe2caa92da660805bb3cdef5318b7c12fbca
prabhleen kaur <prabhleenkaur428@gmail.com>
2024-10-05 18:53:09 +0530

    add basic code

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,8 @@
+function Introduction(){
+    return <div>
+        <div className="h-screen text-4xl flex items-center justify-center">
+            Prabhleen kaur
+        </div>
+    </div>
+}
+export default Introduction;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar (){
+    return (
+        <div className="font-mono font-bold text-lg">
+            <div className="flex justify-between items-center p-4 border-b-2 border-black">
+                Navbar
+                <div className="flex items-center gap-10">
+                    <div>
+                        Projects  
+                </div>
+                    <div>
+                        About me   
+                </div>
+                    <div>
+                        Home  
+                </div>
+                    <div>
+                        Portfolio  
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
